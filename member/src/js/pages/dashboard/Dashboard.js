4561ee5f52d35e17b013e6f054c94a8d438916a9
Philipp Gerber <philipp@rundumeli.ch>
2019-12-17 08:11:21 +0100

    Remove calendar grid chart from dashboard

--- member/src/js/pages/dashboard/Dashboard.js
+++ member/src/js/pages/dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Typography, Grid, Divider, Paper } from '@material-ui/core';
 import { DefaultGrid } from 'Components';
 import { GlobalBalanceChart } from './GlobalBalanceChart';
-import { YearSummaryChart } from './YearSummaryChart';
 
 import styles from './Dashboard.less';
 
@@ -17,7 +16,6 @@ export const Dashboard = () => {
             </Typography>
             <Divider />
             <GlobalBalanceChart />
-            <YearSummaryChart />
           </div>
         </Grid>
       </Grid>
