9c412d652cfa8bcb830daab9d8e4348e31e4f5fb
Philipp Gerber <philipp@rundumeli.ch>
2019-12-22 17:48:57 +0100

    Push missing resolver functions

--- server/api/src/schema/Chat/resolver.js
+++ server/api/src/schema/Chat/resolver.js
@@ -8,6 +8,22 @@ module.exports = {
         skip: args.skip,
       });
     },
+    async unreadMessagesCount(root, args, context) {
+      const { lastSeen } = await context.prisma.chat({ id: args.chatId });
+      let userLastSeen = new Date().toISOString();
+      if (lastSeen && lastSeen[args.userId])
+        userLastSeen = lastSeen[args.userId];
+      console.log(userLastSeen);
+
+      return context.prisma
+        .messagesConnection({
+          where: {
+            AND: [{ chat: { id: args.chatId } }, { date_gt: userLastSeen }],
+          },
+        })
+        .aggregate()
+        .count();
+    },
   },
   Mutation: {
     async createChat(root, args, context) {
@@ -24,6 +40,31 @@ module.exports = {
       });
       console.log(chat);
       return chat;
+    },
+    async userLastSeen(root, args, context) {
+      const { lastSeen } = await context.prisma.chat({ id: args.chatId });
+      const lastSeenUpdated = Object.assign({}, lastSeen, {
+        [args.userId]: new Date().toISOString(),
+      });
+      return context.prisma.updateChat({
+        where: { id: args.chatId },
+        data: {
+          lastSeen: lastSeenUpdated,
+        },
+      });
+    },
+  },
+  Subscription: {
+    onUnreadMessage: {
+      subscribe(root, args, context) {
+        return context.prisma.$subscribe.message({
+          node: { chat: { id: args.chatId } },
+          mutation_in: ['CREATED'],
+        });
+      },
+      resolve: (payload, args, context) => {
+        return payload;
+      },
     },
   },
   Chat: {
