9c924046b686e7b8bea0305997126a7d92ab3307
Philipp Gerber <philipp@rundumeli.ch>
2020-09-06 14:31:00 +0200

    fix: authenticate the prisma client for auth

--- server/api/src/auth/oauth2.js
+++ server/api/src/auth/oauth2.js
@@ -6,6 +6,7 @@ const { Prisma } = require('../generated/prisma-client');
 
 const prisma = new Prisma({
   endpoint: 'http://prisma:4466',
+  secret: process.env.PRISMA_SERVICE_SECRET,
 });
 
 const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION } = process.env;
