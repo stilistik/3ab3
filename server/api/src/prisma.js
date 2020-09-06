5adb341517ea4277795cbbf75d2c0b74b8062215
Philipp Gerber <philipp@rundumeli.ch>
2020-09-06 14:20:43 +0200

    fix: use prisma service secret to initialize prisma client

--- server/api/src/prisma.js
+++ server/api/src/prisma.js
@@ -2,6 +2,7 @@ const { Prisma } = require('./generated/prisma-client');
 
 const prisma = new Prisma({
   endpoint: 'http://prisma:4466',
+  secret: process.env.PRISMA_SERVICE_SECRET,
 });
 
 module.exports = prisma;
