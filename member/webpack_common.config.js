ad1c6e4223649b1ba2da7e2bc00b069a1bf56f9f
Philipp Gerber <philipp@rundumeli.ch>
2020-10-09 17:34:07 +0200

    feat: add support for typescript and wp html plugin

---
+++ member/webpack_common.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        /**
+          TYPESCRIPT SOURCE LOADER
+        */
+        test: /\.ts(x?)$/,
+        include: path.resolve(__dirname, './src'),
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        /**
+          JAVASCRIPT SOURCE LOADER
+        */
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, './src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react'],
+        },
+      },
+      {
+        /**
+          CSS/LESS MODULE LOADER FOR GENERAL PURPOSE
+        */
+        test: /\.(css|less)$/,
+        exclude: path.resolve(__dirname, 'src/css/'),
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'less-loader',
+          },
+        ],
+      },
+      {
+        /**
+          CSS/LESS GLOBAL LOADER
+          Loader for global imports, e.g. master less file
+        */
+        test: /\.(css|less)$/,
+        include: path.resolve(__dirname, 'src/css/'),
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+          },
+        ],
+      },
+      {
+        /**
+          FONT/FILE LOADER
+        */
+        test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      App: path.resolve(__dirname, 'src/js/app'),
+      Pages: path.resolve(__dirname, 'src/js/pages'),
+      Auth: path.resolve(__dirname, 'src/js/auth/'),
+      Components: path.resolve(__dirname, 'src/js/components/'),
+      Routes: path.resolve(__dirname, 'src/js/routes/'),
+      History: path.resolve(__dirname, 'src/js/history/'),
+      Redux: path.resolve(__dirname, 'src/js/redux'),
+      Apollo: path.resolve(__dirname, 'src/js/apollo'),
+      Style: path.resolve(__dirname, 'src/js/style'),
+      Utils: path.resolve(__dirname, 'src/js/utils'),
+    },
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[hash].[name].bundle.js',
+    publicPath: '/',
+  },
+};
---
+++ member/webpack_dev.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const common = require('./webpack_common.config');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '/public/'),
+    port: 3000,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    hotOnly: true,
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __DEPLOYMENT_TARGET__: JSON.stringify('on-prem'),
+      __CERTIFIED__: JSON.stringify(process.env.CERTIFIED === 'true'),
+      __USE_COGNITO__: JSON.stringify(process.env.USE_COGNITO === 'true'),
+    }),
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false }),
+  ],
+});
