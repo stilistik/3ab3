c2d8b3ae7b3d75f4b4558b62382c144633e36533
Philipp Gerber <philipp@rundumeli.ch>
2020-10-10 19:11:23 +0200

    feat: add css baseline to index

--- member/src/index.js
+++ member/src/index.js
@@ -4,17 +4,20 @@ import Style from 'Style';
 import App from 'App';
 import Store from 'Redux/Store';
 import { MuiPickersUtilsProvider as Picker } from 'material-ui-pickers';
+import { CssBaseline } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
 
 import './css/master.less';
 
 ReactDOM.render(
-  <Style>
-    <Store>
-      <Picker utils={DateFnsUtils}>
-        <App />
-      </Picker>
-    </Store>
-  </Style>,
+  <CssBaseline>
+    <Style>
+      <Store>
+        <Picker utils={DateFnsUtils}>
+          <App />
+        </Picker>
+      </Store>
+    </Style>
+  </CssBaseline>,
   document.getElementById('root')
 );
