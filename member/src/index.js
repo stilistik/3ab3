8c9b2c96584bec8333ec0e877e5235fb59202921
Philipp Gerber <philipp@rundumeli.ch>
2020-10-10 23:02:09 +0200

    feat: use correct pickers package

--- member/src/index.js
+++ member/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Style from 'Style';
 import App from 'App';
 import Store from 'Redux/Store';
-import { MuiPickersUtilsProvider as Picker } from 'material-ui-pickers';
+import { MuiPickersUtilsProvider as Picker } from '@material-ui/pickers';
 import { CssBaseline } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
 
