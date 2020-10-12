1558bda418a407305202ab1f34f09ede617ea290
Philipp Gerber <philipp@rundumeli.ch>
2020-10-12 13:53:48 +0200

    feat: add script to create debug session

---
+++ server/misc/debug_session.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+email="&email="$1
+maintenance_password="&password="$2
+credentials=$email$maintenance_password
+echo $credentials
+
+curl -X POST -d $credentials http://localhost:4000/auth/passwordless/debugsession
