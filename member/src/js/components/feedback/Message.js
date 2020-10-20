54893a45b1e49a0fda0bc16fc8ce47f738d841c2
Philipp Gerber <philipp@rundumeli.ch>
2020-10-20 14:04:45 +0200

    use makestyles in message

--- member/src/js/components/feedback/Message.js
+++ member/src/js/components/feedback/Message.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { SnackbarContent, IconButton } from '@material-ui/core';
 import { Icon } from '../icon';
+import { Box } from '../layout';
 import classnames from 'classnames';
+import { makeStyles } from '@material-ui/core/styles';
 import { MessageFormatter } from './MessageFormatter';
 
-import styles from './Message.less';
+const useStyles = makeStyles((theme) => ({
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  success: {
+    backgroundColor: theme.palette.success.main,
+    color: theme.palette.success.contrastText,
+  },
+  error: {
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.success.contrastText,
+  },
+  warning: {
+    backgroundColor: theme.palette.warning.main,
+    color: theme.palette.success.contrastText,
+  },
+  info: {
+    backgroundColor: theme.palette.info.main,
+    color: theme.palette.success.contrastText,
+  },
+}));
 
-const variantIcon = {
+const icons = {
   success: 'checkCircle',
   warning: 'warning',
   error: 'error',
@@ -15,24 +38,22 @@ const variantIcon = {
 
 export const Message = (props) => {
   const { className, message, onClose, variant, ...other } = props;
+  const classes = useStyles();
+
   const msg = MessageFormatter.formatMessage(message);
   return (
     <SnackbarContent
-      style={{ backgroundColor: 'red' }}
-      className={classnames(styles[variant], className)}
+      className={classnames(classes[variant], className)}
       message={
-        <span className={styles.message}>
-          <Icon type={variantIcon[variant]} style={{ marginRight: 5 }} />
+        <span className={classes.message}>
+          <Box mr={1}>
+            <Icon type={icons[variant]} />
+          </Box>
           {msg}
         </span>
       }
       action={[
-        <IconButton
-          key="close"
-          aria-label="close"
-          color="inherit"
-          onClick={onClose}
-        >
+        <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
           <Icon type="close" />
         </IconButton>,
       ]}
