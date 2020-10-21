7667217b73f025c0b97a041a5f3f40fa4e816950
Philipp Gerber <philipp@rundumeli.ch>
2020-10-21 12:43:44 +0200

    feat: generate the prisma client in typescript

--- website/prisma/prisma.ts
+++ website/prisma/prisma.ts
@@ -1,8 +1,7 @@
-import { Prisma } from '../generated/prisma-client';
+import { Prisma } from '../generated/prisma-ts-client';
 
 const { PRISMA_ENDPOINT, PRISMA_SERVICE_SECRET } = process.env;
 
-// @ts-ignore
 export const prisma = new Prisma({
   endpoint: PRISMA_ENDPOINT,
   secret: PRISMA_SERVICE_SECRET,
