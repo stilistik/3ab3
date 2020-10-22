86861395e79915fb709a70072d957fdbc4358f42
Philipp Gerber <philipp@rundumeli.ch>
2020-10-22 16:46:19 +0200

    feat: add react-spring animations for body links

--- website/src/components/layout/Layout.tsx
+++ website/src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import clx from 'classnames';
-
+import { useSpring, animated } from 'react-spring';
 import styles from './Layout.module.css';
 
 const Logo: React.FC = () => {
@@ -17,28 +17,72 @@ const Logo: React.FC = () => {
 };
 
 interface HeaderLinkProps {
-  pathName: string;
+  pathname: string;
 }
 
-const HeaderLink: React.FC<HeaderLinkProps> = ({ children, pathName }) => {
+const HeaderLink: React.FC<HeaderLinkProps> = ({ pathname, children }) => {
   const router = useRouter();
-  const isActive = router.pathname === pathName;
-  const className = clx(styles.link, { [styles.active]: isActive });  
+  const cls = clx(styles.link, {
+    [styles.active]: router.pathname === pathname,
+  });
   return (
-    <Link href={pathName}>
-      <a className={className}>{children}</a>
+    <Link href={pathname}>
+      <a className={cls}>{children}</a>
     </Link>
+  );
+};
+
+interface BodyLinkProps {
+  side: 'right' | 'left';
+}
+
+const BodyLink: React.FC<BodyLinkProps> = ({ side, children }) => {
+  const point = side === 'left' ? 100 : -100;
+
+  const [props, set] = useSpring(() => ({
+    x: point,
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
+  const trans = (y: number) => {
+    return `translate3d(0px, ${y}px, 0px)`;
+  };
+
+  return (
+    <div
+      className={`fixed inset-y-0 ${side}-0 flex items-center justify-center z-10`}
+      style={{ width: 200 }}
+    >
+      <div
+        className="transform rotate-90 cursor-pointer"
+        style={{ fontSize: 180 }}
+      >
+        <animated.div
+          style={{ transform: props.x.interpolate(trans) }}
+          onMouseEnter={() => set({ x: 0 })}
+          onMouseLeave={() => set({ x: point })}
+        >
+          <div className="font-black uppercase leading-none select-none">
+            <span className={styles.bodyLink}>{children}</span>
+          </div>
+        </animated.div>
+      </div>
+    </div>
   );
 };
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <HeaderLink pathName="/">Events</HeaderLink>
-      <HeaderLink pathName="/contact">Kontakt</HeaderLink>
+      <div className="mx-10">
+        <HeaderLink pathname="/">Events</HeaderLink>
+        <HeaderLink pathname="/contact">Kontakt</HeaderLink>
+      </div>
       <Logo />
-      <HeaderLink pathName="/about">Verein</HeaderLink>
-      <HeaderLink pathName="/archive">Archiv</HeaderLink>
+      <div className="mx-10">
+        <HeaderLink pathname="/statutes">Statuten</HeaderLink>
+        <HeaderLink pathname="/archive">Archiv</HeaderLink>
+      </div>
     </header>
   );
 };
@@ -47,9 +91,9 @@ export const Layout: React.FC = ({ children }) => {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <div className={styles.back} />
+      <BodyLink side="left">Archiv</BodyLink>
       <main className={styles.main}>{children}</main>
-      <div className={styles.forward} />
+      <BodyLink side="right">Kontakt</BodyLink>
     </div>
   );
 };
