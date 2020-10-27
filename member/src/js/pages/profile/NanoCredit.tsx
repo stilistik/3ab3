8e7a7a88df089c6adb584201a0a0c626dfa0be76
Philipp Gerber <philipp@rundumeli.ch>
2020-10-27 16:52:56 +0100

    feat: translate nanokredit explanation

--- member/src/js/pages/profile/NanoCredit.tsx
+++ member/src/js/pages/profile/NanoCredit.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import { Box, Form, NumberField } from 'Components/index';
+import { Paper, Typography, Button } from '@material-ui/core';
+import { useTranslation, Trans } from 'react-i18next';
+import { Serializable, FieldOptions } from 'Components/form/types';
+
+interface NanoCreditProps {
+  onSubmit: (
+    values: NestedRecord<Serializable>,
+    options: NestedRecord<FieldOptions>
+  ) => void;
+}
+
+const NanoCreditForm: React.FC<NanoCreditProps> = ({ onSubmit }) => {
+  const { t } = useTranslation();
+  return (
+    <Form onSubmit={onSubmit}>
+      <Box cmb={1}>
+        <NumberField id="amount" label={t('Amount')} />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          style={{ marginTop: 10 }}
+        >
+          {t('Take out credit')}
+        </Button>
+      </Box>
+    </Form>
+  );
+};
 
 export const NanoCredit: React.FC = () => {
+  const { t } = useTranslation();
+
+  const handleSubmit = (values: NestedRecord<Serializable>) => {};
+
   return (
     <Paper>
-      <h1>hello</h1>
+      <Box p={2}>
+        <Typography variant="h6">{t('Nano Credit')}</Typography>
+        <Typography variant="body2">
+          <Trans i18nKey="nanoCreditExplanation">
+            Here you can take out a nano credit. After registering it, you can
+            take the corresponding amount from the cash register and use it as
+            you please. The maximum amount you can take out is 50 CHF.
+          </Trans>
+        </Typography>
+        <NanoCreditForm onSubmit={handleSubmit} />
+      </Box>
     </Paper>
   );
 };
