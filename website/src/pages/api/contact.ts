7de835cae696f6ad6c01e9a90a13336730bc1aa0
Philipp Gerber <philipp@rundumeli.ch>
2020-10-29 15:23:40 +0100

    feat: hook up contact form with mailgun

--- website/src/pages/api/contact.ts
+++ website/src/pages/api/contact.ts
@@ -1,6 +1,19 @@
 import Axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const {
+  RECAPTCHA_PRIVATE_KEY,
+  CONTACT_EMAIL_ADDRESS,
+  MAILGUN_API_SECRET,
+  MAILGUN_DOMAIN,
+} = process.env;
+
+const mailgun = require('mailgun-js')({
+  apiKey: MAILGUN_API_SECRET,
+  domain: MAILGUN_DOMAIN,
+  host: 'api.eu.mailgun.net',
+});
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { token, name, email, subject, message } = JSON.parse(req.body);
@@ -8,7 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const googleUrl =
       'https://www.google.com/recaptcha/api/siteverify?secret=' +
-      process.env.RECAPTCHA_PRIVATE_KEY +
+      RECAPTCHA_PRIVATE_KEY +
       '&response=' +
       token;
 
@@ -19,17 +32,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (captchaResponse.data.success === false) {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
-      return res.end(JSON.stringify({ success: false, message: 'captcha failed' }));
-    } else {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
       return res.end(
-        JSON.stringify({
-          success: true,
-          message: 'captcha approved',
-        })
+        JSON.stringify({ success: false, message: 'captcha failed' })
       );
     }
+
+    const data = {
+      from: `3ab3 Contact Form <${email}>`,
+      to: CONTACT_EMAIL_ADDRESS,
+      subject: subject,
+      text: message,
+    };
+    mailgun.messages().send(data);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(
+      JSON.stringify({
+        success: true,
+        message: 'Message sent',
+      })
+    );
   }
-  return res.status(200).json({ name: 'John Doe' });
 };
