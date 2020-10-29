895ecb99f9892d45f2da7ca7599e90832ec37065
Philipp Gerber <philipp@rundumeli.ch>
2020-10-29 09:52:44 +0100

    add slight shadow to logo

--- website/src/components/layout/Header.tsx
+++ website/src/components/layout/Header.tsx
@@ -25,8 +25,8 @@ const Logo: React.FC = () => {
   return (
     <Link href="/">
       <div className={styles.logo}>
-        <h1>3A</h1>
-        <h1>B3</h1>
+        <h1 className="shadow-2xl">3A</h1>
+        <h1 className="shadow-2xl">B3</h1>
       </div>
     </Link>
   );
