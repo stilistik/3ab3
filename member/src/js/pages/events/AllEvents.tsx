55f5bc44f4eec92e209293e0fe6e6b371e5caa2a
Philipp Gerber <philipp@rundumeli.ch>
2020-11-02 17:49:44 +0100

    fix: remove unused styles

--- member/src/js/pages/events/AllEvents.tsx
+++ member/src/js/pages/events/AllEvents.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { Masonry, EventCard, Grid, Box } from 'Components/index';
 import { usePaginatedQuery } from 'Components/utility/usePaginatedQuery';
-import { Button, makeStyles } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { ALL_EVENTS_FEED } from 'Graphql/queries';
 import { Event } from 'Graphql/types';
-
-const useStyles = makeStyles((theme) => ({
-  grid: {
-    display: 'flex',
-    marginLeft: -theme.spacing(3) /* gutter size offset */,
-    width: 'auto',
-  },
-  column: {
-    paddingLeft: theme.spacing(3) /* gutter size */,
-    backgroundClip: 'padding-box',
-  },
-}));
 
 export const AllEvents = () => {
   const {
@@ -26,7 +14,6 @@ export const AllEvents = () => {
     hasNext,
     cursor,
   } = usePaginatedQuery<Event>(ALL_EVENTS_FEED, 10);
-  const styles = useStyles();
 
   if (loading || error) return null;
 
