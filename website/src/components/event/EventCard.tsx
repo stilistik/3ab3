4b8eae6948124a5bf6b432a73863f3dc1391866e
David Malesevic <malesevic.dm@gmail.com>
2020-11-03 18:00:10 +0100

    conditionally renderig event card shiz

--- website/src/components/event/EventCard.tsx
+++ website/src/components/event/EventCard.tsx
@@ -13,8 +13,12 @@ interface ShowMoreButtonProps {
   isActive: boolean;
 }
 
-const ShowMoreButton: React.FC<ShowMoreButtonProps> = ({ onClick, className, btnText, isActive }) => {
-
+const ShowMoreButton: React.FC<ShowMoreButtonProps> = ({
+  onClick,
+  className,
+  btnText,
+  isActive,
+}) => {
   const moreBtn = clx(className, styles.topRight, styles.more, {
     [styles.slanted]: isActive,
   });
@@ -35,13 +39,11 @@ interface EventCardProps {
 }
 
 export const EventCard: React.FC<EventCardProps> = ({ event }) => {
-
   const [isActive, setActive] = React.useState(false);
 
   const handleClick = () => {
     setActive((isActive) => !isActive);
   };
-  
 
   return (
     <Container>
@@ -50,23 +52,28 @@ export const EventCard: React.FC<EventCardProps> = ({ event }) => {
           src={event.image}
           className={styles.wrapper + ' shadow-2xl'}
         >
-          <div>
+          <div className={styles.topLeft}>
             <h2 className={styles.date}>
               {new Date(event.date).toLocaleDateString()}
             </h2>
             <h4 className={styles.time}>
               {'Doors: ' + new Date(event.date).toLocaleTimeString()}
             </h4>
+          </div>
+          <div className={styles.topRight}>
             <ShowMoreButton
               onClick={handleClick}
               className="absolute top-0 right-0 mr-3 mt-3"
               btnText="+"
               isActive={isActive}
             />
-            <div className={styles.bottomRight}>
-              <h1 className={styles.title}>{event.title}</h1>
-              <h4 className={styles.subtitle}>{event.subtitle}</h4>
-            </div>
+          </div>
+          <div className="midRight">
+            {isActive ? <p>es ist aktiv</p> : <p>es ist nicht aktiv</p>}
+          </div>
+          <div className={styles.bottomRight}>
+            <h1 className={styles.title}>{event.title}</h1>
+            <h4 className={styles.subtitle}>{event.subtitle}</h4>
           </div>
         </LazyLoadingImageDiv>
       </AspectRatioBox>
