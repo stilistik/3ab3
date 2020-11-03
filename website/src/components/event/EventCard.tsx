a07281b316656e818ca00990c63c85d51e4aefc9
David Malesevic <malesevic.dm@gmail.com>
2020-11-03 16:56:48 +0100

    Event Card Show more button text as prop

--- website/src/components/event/EventCard.tsx
+++ website/src/components/event/EventCard.tsx
@@ -8,21 +8,27 @@ import styles from './EventCard.module.css';
 
 interface ShowMoreProps {
   className: string;
+  btnText: string;
 }
 
-const ShowMore: React.FC<ShowMoreProps> = ({ className }) => {
+const ShowMoreButton: React.FC<ShowMoreProps> = ({ className, btnText }) => {
   const [isActive, setActive] = React.useState(false);
 
   const handleClick = () => {
     setActive((isActive) => !isActive);
   };
 
-  const moreBtn = clx(className, styles.more, {
+  const moreBtn = clx(className, styles.topRight, styles.more, {
     [styles.slanted]: isActive,
   });
+
+  const moreWrapper = clx(styles.fillRight);
+
+  console.log(moreWrapper);
+
   return (
     <button onClick={handleClick} className={moreBtn}>
-      +
+      {btnText}
     </button>
   );
 };
@@ -39,18 +45,21 @@ export const EventCard: React.FC<EventCardProps> = ({ event }) => {
           src={event.image}
           className={styles.wrapper + ' shadow-2xl'}
         >
-          <ShowMore className="absolute top-0 right-0 mr-3 mt-3" />
-          <div className={styles.top}>
+          <div>
             <h2 className={styles.date}>
               {new Date(event.date).toLocaleDateString()}
             </h2>
             <h4 className={styles.time}>
               {'Doors: ' + new Date(event.date).toLocaleTimeString()}
             </h4>
-          </div>
-          <div className={styles.bottom}>
-            <h1 className={styles.title}>{event.title}</h1>
-            <h4 className={styles.subtitle}>{event.subtitle}</h4>
+            <ShowMoreButton
+              className="absolute top-0 right-0 mr-3 mt-3"
+              btnText="+"
+            />
+            <div className={styles.bottomRight}>
+              <h1 className={styles.title}>{event.title}</h1>
+              <h4 className={styles.subtitle}>{event.subtitle}</h4>
+            </div>
           </div>
         </LazyLoadingImageDiv>
       </AspectRatioBox>
