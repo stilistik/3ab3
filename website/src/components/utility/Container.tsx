dd7cf878cb6087b58dacfc15338d4efbdbe22021
Philipp Gerber <philipp@rundumeli.ch>
2020-11-12 11:52:47 +0100

    support className prop on container

--- website/src/components/utility/Container.tsx
+++ website/src/components/utility/Container.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import clx from 'classnames';
 import styles from './Container.module.css';
 
-export const Container: React.FC = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+interface ContainerProps {
+  className?: string;
+}
+
+export const Container: React.FC<ContainerProps> = ({
+  className,
+  children,
+}) => {
+  return <div className={clx(styles.container, className)}>{children}</div>;
 };
