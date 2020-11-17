051bc4ee3b2927a983345a751ec7845f226458e6
Philipp Gerber <philipp@rundumeli.ch>
2020-11-17 09:18:59 +0100

    update and improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,24 @@
 echo "Pulling latest changes from master..."
 git checkout master && git pull origin master
 
-echo "Stopping container..."
-make prod/all.stop 
+echo "Stopping containers..."
+docker-compose -f docker-compose.prod.yml down
 
 echo "Exporting environment"
 export $(grep -v '^#' .env | xargs)   
 
-echo "Building ..."
-make prod/all.run-rebuild
+echo "Packaging and starting services..."
+docker-compose -f docker-compose.prod.yml build postgres prisma proxy api
+docker-compose -f docker-compose.prod.yml up -d postgres prisma proxy api 
+
+echo "Building member client"
+cd member && yarn build 
+
+echo "Building public website"
+cd website && yarn build 
+
+echo "Packaging and starting clients"
+docker-compose -f docker-compose.prod.yml build member website
+docker-compose -f docker-compose.prod.yml up -d member website
 
 exit
