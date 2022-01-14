18aa19e5247f1dd3094fb18d18e8c319867593dd
Philipp Gerber <philipp@retinai.com>
2022-01-14 15:14:13 +0100

    feat: add delete mutation for documents

--- server/api/src/schema/Document/resolver.js
+++ server/api/src/schema/Document/resolver.js
@@ -1,5 +1,9 @@
 const { verifyAndDecodeToken } = require('../../auth/verify');
-const { uploadFile, createPdfThumbnail } = require('../../helper/file.helper');
+const {
+  uploadFile,
+  deleteFile,
+  createPdfThumbnail,
+} = require('../../helper/file.helper');
 
 module.exports = {
   Query: {
@@ -26,6 +30,28 @@ module.exports = {
         where: { id: args.documentId },
         data: args.input,
       });
+    },
+    async deleteDocument(root, args, context) {
+      const doc = await context.prisma.document({
+        id: args.documentId,
+      });
+      if (doc) {
+        const file = await context.prisma
+          .document({ id: args.documentId })
+          .file();
+
+        const thumbnailFile = await context.prisma.file({ uri: doc.thumbnail });
+
+        await context.prisma.deleteDocument({ id: doc.id });
+        if (file) {
+          await deleteFile(file.id, context);
+        }
+        if (thumbnailFile) {
+          await deleteFile(thumbnailFile.id, context);
+        }
+      }
+
+      return Boolean(doc);
     },
   },
   Document: {
